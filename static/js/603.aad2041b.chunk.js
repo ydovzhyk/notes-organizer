4b573db1b86738fd12741eb367dc5cc779718eb9
ydovzhyk <48929257+ydovzhyk@users.noreply.github.com>
2024-01-09 14:21:02 +0000

    Deploying to gh-pages from @ ydovzhyk/notes-organizer@51650e38044c83c1b489b7e01ad3b59f6126d4c4 🚀

---
+++ static/js/603.aad2041b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknotes_organizer=self.webpackChunknotes_organizer||[]).push([[603],{7603:function(n,e,r){r.r(e),r.d(e,{default:function(){return c}});var t=r(7689),u=r(3329),s=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(t.j3,{})})},c=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(s,{})})}}}]);
+//# sourceMappingURL=603.aad2041b.chunk.js.map
