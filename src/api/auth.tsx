9e90660083d99244a5b64f96303ed64ee2086d9c
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-01-26 14:27:49 +0200

    Update auth.tsx

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -11,8 +11,8 @@ import {
   IAuth,
 } from '../components/types/auth/axios-auth';
 
-const REACT_APP_API_URL = 'http://localhost:4000';
-// const REACT_APP_API_URL = 'https://notes-organizer-backend-20095b30617d.herokuapp.com/';
+// const REACT_APP_API_URL = 'http://localhost:4000';
+const REACT_APP_API_URL = 'https://notes-organizer-backend-20095b30617d.herokuapp.com/';
 
 export const instance = axios.create({
   baseURL: REACT_APP_API_URL,
