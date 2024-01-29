fd0434f0871ae29e7b1a8bde9af144c2f6ab330a
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-01-29 20:12:46 +0200

    add 404
    add 404

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,6 @@ import './styles/styles.scss';
 
 const basename = process.env.PUBLIC_URL || '/';
 
-console.log('Це basename', basename);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter basename={basename}>
     <Provider store={store}>
@@ -23,31 +21,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </Provider>
   </BrowserRouter>
 );
-
-// if (process.env.NODE_ENV === 'production') {
-//   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//     <HashRouter>
-//       <Provider store={store}>
-//         <PersistGate loading={<Loader />} persistor={persistor}>
-//           <App />
-//         </PersistGate>
-//       </Provider>
-//     </HashRouter>
-//   );
-// }
-
-// if (process.env.NODE_ENV === 'development') {
-//   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//     <BrowserRouter>
-//       <Provider store={store}>
-//         <PersistGate loading={<Loader />} persistor={persistor}>
-//           <App />
-//         </PersistGate>
-//       </Provider>
-//     </BrowserRouter>
-//   );
-// }
-
 
 //потрібно додати react-router-dom, redux, і redux-persist до додатку та налаштувати їх, а також включити роутер та редукс у вашому вихідному файлі index.tsx.
 //Встановіть необхідні залежності:
