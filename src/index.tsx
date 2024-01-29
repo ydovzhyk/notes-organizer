f59d5e6986055fd7a56c7bcb269286210887545d
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-01-29 20:51:18 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './Redux/store'; 
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, HashRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor, store } from './Redux/store'; 
 
-import './index.css';
+// import './index.css';
 
-import App from './App';
-import Loader from './components/Loader/Loader';
-import './styles/styles.scss';
+// import App from './App';
+// import Loader from './components/Loader/Loader';
+// import './styles/styles.scss';
 
 // const basename = process.env.PUBLIC_URL || '/';
 
@@ -22,29 +22,53 @@ import './styles/styles.scss';
 //   </BrowserRouter>
 // );
 
-if (process.env.NODE_ENV === 'production') {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <HashRouter>
-      <Provider store={store}>
-        <PersistGate loading={<Loader />} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </HashRouter>
-  );
-}
+// if (process.env.NODE_ENV === 'production') {
+//   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//     <HashRouter>
+//       <Provider store={store}>
+//         <PersistGate loading={<Loader />} persistor={persistor}>
+//           <App />
+//         </PersistGate>
+//       </Provider>
+//     </HashRouter>
+//   );
+// }
 
-if (process.env.NODE_ENV === 'development') {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate loading={<Loader />} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
-  );
-}
+// if (process.env.NODE_ENV === 'development') {
+//   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//     <BrowserRouter>
+//       <Provider store={store}>
+//         <PersistGate loading={<Loader />} persistor={persistor}>
+//           <App />
+//         </PersistGate>
+//       </Provider>
+//     </BrowserRouter>
+//   );
+// }
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './Redux/store'; 
+
+import './index.css';
+
+import App from './App';
+import Loader from './components/Loader/Loader';
+import './styles/styles.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </Router>
+);
 
 
 //потрібно додати react-router-dom, redux, і redux-persist до додатку та налаштувати їх, а також включити роутер та редукс у вашому вихідному файлі index.tsx.
